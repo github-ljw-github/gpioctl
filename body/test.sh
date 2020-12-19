cd041d20990649297bd52e212c62bcc1d5d7621d
github-ljw-github <qmail_ljw_qmail@foxmail.com>
2020-12-19 05:37:21 -0500

    add more test case
    Signed-off-by: github-ljw-github <qmail_ljw_qmail@foxmail.com>

--- body/test.sh
+++ body/test.sh
@@ -7,17 +7,32 @@ function test_init
 
 function test_read_gpio
 {
+  echo "read: test case 1"
   GPIO_PortName="1|2|7 |2"
   GPIO_PinNumbr="2|3|23|25"
   ./a.out "$GPIO_PortName" "$GPIO_PinNumbr"
+
+
+  echo "read: test case 2"
+  ./a.out "gpio1|gpio2|gpio7|gpio7"\
+          "pin2 |pin3 |pin23|pin25"\
+
 }
 
 function test_write_gpio
 {
+  echo "write: test case 1"
   GPIO_PortName="1|2|7 |2"
   GPIO_PinNumbr="2|3|23|25"
   GPIO_PinValue="0|1|1 |0 "
   ./a.out "$GPIO_PortName" "$GPIO_PinNumbr" "$GPIO_PinValue"
+
+
+  echo "write: test case 2"
+  ./a.out "gpio1|gpio2|gpio7|gpio7"\
+          "pin2 |pin3 |pin23|pin25"\
+          "val0 |val1 |val0 |val1"\
+
 }
 
 test_init
